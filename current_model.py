01646bf5c78a2f504a0e6b637892385538946d0a
Marek K. Zielinski <marek.k.zielinski@outlook.com>
2017-12-19 23:29:28 +0100

    Simpler arhitecture - less Conv2D layers

--- current_model.py
+++ current_model.py
@@ -5,9 +5,6 @@ aux_input = Input(shape=(75,75,3), name='aux_input')
 #conv layers for main_input
 x1 = BatchNormalization()(main_input)
 x1 = Conv2D(64, (3,3), activation='relu')(x1)
-x1 = BatchNormalization()(x1)
-x1 = Dropout(0.2)(x1)
-x1 = Conv2D(64, (3, 3), activation='relu')(x1)
 x1 = MaxPooling2D((2, 2), strides=(2, 2))(x1)
 x1 = BatchNormalization()(x1)
 x1 = Dropout(0.2)(x1)
@@ -15,9 +12,6 @@ x1 = Dropout(0.2)(x1)
 #conv layers for aux_input
 x2 = BatchNormalization()(aux_input)
 x2 = Conv2D(64, (3,3), activation='relu')(x2)
-x2 = BatchNormalization()(x2)
-x2 = Dropout(0.2)(x2)
-x2 = Conv2D(64, (3, 3), activation='relu')(x2)
 x2 = MaxPooling2D((2, 2), strides=(2, 2))(x2)
 x2 = BatchNormalization()(x2)
 x2 = Dropout(0.2)(x2)
@@ -26,9 +20,12 @@ x = Concatenate(axis=3)([x1,x2])
     
     #conv-block
 x = Conv2D(128, (3, 3), activation='relu')(x)
+x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
-x = Conv2D(128, (3, 3), activation='relu')(x)
+
+    #conv-block
+x = Conv2D(256, (3, 3), activation='relu')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
