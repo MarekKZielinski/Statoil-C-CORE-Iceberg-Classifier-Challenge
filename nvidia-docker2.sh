85bc0143b44b9c2fb310aef4d30a7717c7993d09
Marek K. Zielinski <marek.k.zielinski@outlook.com>
2017-12-20 23:19:14 +0100

    Not a good candidate

--- nvidia-docker2.sh
+++ nvidia-docker2.sh
@@ -1,1 +1,13 @@
-jupyter notebook --no-browser --ip=0.0.0.0 --allow-root
+apt-get update
+apt-get install screen
+pip3 install keras
+apt-get install libcupti-dev
+pip3 install --upgrade tensorflow-gpu
+pip3 install scikit-image
+apt-get install git
+git config user.email "marek.k.zielinski@outlook.com"
+git config --global user.name "Marek K. Zielinski"
+apt-get install htop
+pip3 install pydot
+apt-get install graphviz
+pip3 install graphviz
