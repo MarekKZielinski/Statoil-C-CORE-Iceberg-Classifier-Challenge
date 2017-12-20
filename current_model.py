b1d3eac9911f004c9593816b4691dfbe5cb0cc10
Marek K. Zielinski <marek.k.zielinski@outlook.com>
2017-12-20 21:43:36 +0100

    Added inception module - less inputs

--- current_model.py
+++ current_model.py
@@ -1,31 +1,43 @@
 
-def InputBlock(x):
+def InceptionBlock(x, n_kernels=32, prefix=''):
+    path1 = Conv2D(n_kernels, (1,1), padding='same', activation='relu', name=prefix + '11_Conv_1_1')(x)
+    
+    path2 = Conv2D(n_kernels, (1,1), padding='same', activation='relu', name=prefix + '21_Conv_1_1')(x)
+    path2 = Conv2D(n_kernels, (3,3), padding='same', activation='relu', name=prefix + '22_Conv_3_3')(path2)
+    
+    path3 = Conv2D(n_kernels, (1,1), padding='same', activation='relu', name=prefix + '31_Conv_1_1')(x)
+    path3 = Conv2D(n_kernels, (5,5), padding='same', activation='relu', name=prefix + '32_Conv_5_5')(path3)
+    
+    path4 = MaxPooling2D((3,3), strides=(1,1), padding='same', name=prefix + '41_MaxPool_3_3')(x)
+    path4 = Conv2D(n_kernels, (1,1), padding='same', activation='relu', name=prefix + '42_Conv_1_1')(path4)
+    
+    out = Concatenate(axis=3, name=prefix+'_Inception_end')([path1,path2,path3,path4])
+    return(out)
+
+def InputBlock(x, dropout=0.2, prefix=''):
     #conv layers for input
     x = BatchNormalization()(x)
     x = Conv2D(64, (3,3), activation='relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2))(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.2)(x) 
+    x = Dropout(dropout)(x) 
+    x = InceptionBlock(x,prefix=prefix)
     return(x)
 
 main_input = Input(shape=(75,75,2), name='main_input')
 aux_input = Input(shape=(75,75,3), name='aux_input')
-aux_input_wavelet = Input(shape=(75,75,4), name='aux_input_wavelet')
 aux_input_nn = Input(shape=(75,75,4), name='aux_input_nn')
-aux_input_filter = Input(shape=(75,75,4), name='aux_input_filter')
-aux_input_dilated = Input(shape=(75,75,4), name='aux_input_dilated')
 
-x1 = InputBlock(main_input)
-x2 = InputBlock(aux_input)
-x3 = InputBlock(aux_input_wavelet)
-x4 = model_denoise(aux_input_nn)
-x4 = InputBlock(x4)
-x5 = InputBlock(aux_input_filter)
-x6 = InputBlock(aux_input_dilated)
+x1 = InputBlock(main_input, prefix='m_input')
+x2 = InputBlock(aux_input, prefix='a_input')
+x3 = model_denoise(aux_input_nn)
+x3 = InputBlock(x3,dropout=0.5, prefix='a_input_nn')
 
-x = Concatenate(axis=3)([x1,x2,x3,x4,x5,x6])
+x = Concatenate(axis=3)([x1,x2,x3])
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
+
+x = InceptionBlock(x, prefix='main_path')
 
 #conv-block
 x = Conv2D(128, (3, 3), activation='relu')(x)
@@ -57,7 +69,7 @@ x = Dropout(0.2)(x)
 
 main_output = Dense(1, activation='sigmoid', name='main_output')(x)
 model_f = Model(inputs=[main_input,aux_input, 
-                        aux_input_wavelet, aux_input_nn, aux_input_filter, aux_input_dilated,
+                        aux_input_nn, 
                         angle_input,], 
                         outputs=[main_output])
 
