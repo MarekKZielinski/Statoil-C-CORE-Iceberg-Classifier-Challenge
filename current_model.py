37e4e2447c7e852551ed11aa44bfcc6b4bc60031
Marek K. Zielinski <marek.k.zielinski@outlook.com>
2017-12-20 15:28:47 +0100

    model comment: 2 CNN inputs 3,3 conv filters - denoised and filtered images as additional inputs Min validation loss epoch: epoch: 43 ; val loss [min] 0.2082:  ; training loss: 0.1843 ; val acc: 0.9199 ; training acc: 0.9269 Max validation accuracy epoch: epoch: 43 ; val loss 0.2082:  ; training loss: 0.1843 ; val acc [max]: 0.9199 ; training acc: 0.9269

--- current_model.py
+++ current_model.py
@@ -1,23 +1,29 @@
 
+def InputBlock(x):
+    #conv layers for input
+    x = BatchNormalization()(x)
+    x = Conv2D(64, (3,3), activation='relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.2)(x) 
+    return(x)
+
 main_input = Input(shape=(75,75,2), name='main_input')
 aux_input = Input(shape=(75,75,3), name='aux_input')
+aux_input_wavelet = Input(shape=(75,75,4), name='aux_input_wavelet')
+aux_input_nn = Input(shape=(75,75,4), name='aux_input_nn')
+aux_input_filter = Input(shape=(75,75,4), name='aux_input_filter')
 
-#conv layers for main_input
-x1 = BatchNormalization()(main_input)
-x1 = Conv2D(64, (3,3), activation='relu')(x1)
-x1 = MaxPooling2D((2, 2), strides=(2, 2))(x1)
-x1 = BatchNormalization()(x1)
-x1 = Dropout(0.2)(x1)
+x1 = InputBlock(main_input)
+x2 = InputBlock(aux_input)
+x3 = InputBlock(aux_input_wavelet)
+x4 = InputBlock(aux_input_nn)
+x5 = InputBlock(aux_input_filter)
 
-#conv layers for aux_input
-x2 = BatchNormalization()(aux_input)
-x2 = Conv2D(64, (3,3), activation='relu')(x2)
-x2 = MaxPooling2D((2, 2), strides=(2, 2))(x2)
-x2 = BatchNormalization()(x2)
-x2 = Dropout(0.2)(x2)
+x = Concatenate(axis=3)([x1,x2,x3,x4,x5])
+x = BatchNormalization()(x)
+x = Dropout(0.2)(x)
 
-x = Concatenate(axis=3)([x1,x2])
-    
 #conv-block
 x = Conv2D(128, (3, 3), activation='relu')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2))(x)
@@ -33,7 +39,8 @@ x = Dropout(0.2)(x)
 #flatten
 x = Flatten()(x)
 angle_input = Input(shape=[1], name='angle_input')
-merged = Concatenate()([x, angle_input])
+x1 = BatchNormalization()(angle_input)
+merged = Concatenate()([x, x1])
 
 #dense-block
 x = Dense(513, activation='relu')(merged)
@@ -46,7 +53,9 @@ x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
 
 main_output = Dense(1, activation='sigmoid', name='main_output')(x)
-model_f = Model(inputs=[main_input,aux_input,angle_input], outputs=[main_output])
+model_f = Model(inputs=[main_input,aux_input, aux_input_wavelet, aux_input_nn, aux_input_filter,
+                        angle_input,], 
+                        outputs=[main_output])
 
 model_f.compile(optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0),
 loss='binary_crossentropy',
