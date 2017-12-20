d56e14ba6cc6cb8660c22275988e908093c4961e
Marek K. Zielinski <marek.k.zielinski@outlook.com>
2017-12-20 03:15:56 +0100

    model comment: 2 CNN inputs 5,5 conv filters Min validation loss epoch: epoch: 49 ; val loss [min] 0.1799:  ; training loss: 0.1805 ; val acc: 0.9189 ; training acc: 0.9287 Max validation accuracy epoch: epoch: 45 ; val loss 0.1827:  ; training loss: 0.2124 ; val acc [max]: 0.9372 ; training acc: 0.9118

--- current_model.py
+++ current_model.py
@@ -4,14 +4,14 @@ aux_input = Input(shape=(75,75,3), name='aux_input')
 
 #conv layers for main_input
 x1 = BatchNormalization()(main_input)
-x1 = Conv2D(64, (3,3), activation='relu')(x1)
+x1 = Conv2D(64, (5,5), activation='relu')(x1)
 x1 = MaxPooling2D((2, 2), strides=(2, 2))(x1)
 x1 = BatchNormalization()(x1)
 x1 = Dropout(0.2)(x1)
 
 #conv layers for aux_input
 x2 = BatchNormalization()(aux_input)
-x2 = Conv2D(64, (3,3), activation='relu')(x2)
+x2 = Conv2D(64, (5,5), activation='relu')(x2)
 x2 = MaxPooling2D((2, 2), strides=(2, 2))(x2)
 x2 = BatchNormalization()(x2)
 x2 = Dropout(0.2)(x2)
@@ -19,15 +19,13 @@ x2 = Dropout(0.2)(x2)
 x = Concatenate(axis=3)([x1,x2])
     
 #conv-block
-x = ZeroPadding2D()(x)
-x = Conv2D(128, (3, 3), activation='relu')(x)
+x = Conv2D(128, (5, 5), activation='relu')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
 
 #conv-block
-x = ZeroPadding2D()(x)
-x = Conv2D(256, (3, 3), activation='relu')(x)
+x = Conv2D(256, (5, 5), activation='relu')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
