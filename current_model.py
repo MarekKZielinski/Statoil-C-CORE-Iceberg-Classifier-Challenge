2db21ea23490aad8b5788d776a26decaeb4de50a
Marek K. Zielinski <marek.k.zielinski@outlook.com>
2017-12-20 20:24:36 +0100

    model comment: 2 CNN inputs 3,3 conv filters - added dilated images to inputs Min validation loss epoch: epoch: 53 ; val loss [min] 0.2294:  ; training loss: 0.2350 ; val acc: 0.9006 ; training acc: 0.9055 Max validation accuracy epoch: epoch: 49 ; val loss 0.2314:  ; training loss: 0.2347 ; val acc [max]: 0.9060 ; training acc: 0.9109 Overall - to complex or to little data

--- current_model.py
+++ current_model.py
@@ -13,14 +13,17 @@ aux_input = Input(shape=(75,75,3), name='aux_input')
 aux_input_wavelet = Input(shape=(75,75,4), name='aux_input_wavelet')
 aux_input_nn = Input(shape=(75,75,4), name='aux_input_nn')
 aux_input_filter = Input(shape=(75,75,4), name='aux_input_filter')
+aux_input_dilated = Input(shape=(75,75,4), name='aux_input_dilated')
 
 x1 = InputBlock(main_input)
 x2 = InputBlock(aux_input)
 x3 = InputBlock(aux_input_wavelet)
-x4 = InputBlock(aux_input_nn)
+x4 = model_denoise(aux_input_nn)
+x4 = InputBlock(x4)
 x5 = InputBlock(aux_input_filter)
+x6 = InputBlock(aux_input_dilated)
 
-x = Concatenate(axis=3)([x1,x2,x3,x4,x5])
+x = Concatenate(axis=3)([x1,x2,x3,x4,x5,x6])
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
 
@@ -53,7 +56,8 @@ x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
 
 main_output = Dense(1, activation='sigmoid', name='main_output')(x)
-model_f = Model(inputs=[main_input,aux_input, aux_input_wavelet, aux_input_nn, aux_input_filter,
+model_f = Model(inputs=[main_input,aux_input, 
+                        aux_input_wavelet, aux_input_nn, aux_input_filter, aux_input_dilated,
                         angle_input,], 
                         outputs=[main_output])
 
