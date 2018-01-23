3c04afcf2020d637765e325c3b492e1d2e7fe5bb
Marek.K.Zielinski <marek.k.zielinski@outlook.com>
2018-01-23 17:16:29 +0000

    added new models with noise

--- current_model.py
+++ current_model.py
@@ -1,15 +1,12 @@
 
-def InputBlock(x, dropout=0.25, prefix=''):
+def InputBlock(x, dropout=0.2, prefix=''):
     #conv layers for input
     x = BatchNormalization()(x)
     x = Conv2D(64, (3,3), activation='relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2))(x)
     x = BatchNormalization()(x)
     x = Dropout(dropout)(x) 
-<<<<<<< HEAD
     x = BatchNormalization()(x)
-=======
->>>>>>> 4761cbec63aa7900b08cd60db6accbcaf4e0fa84
     x = Conv2D(64, (3,3), activation='relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2))(x)
     x = BatchNormalization()(x)
@@ -17,22 +14,16 @@ def InputBlock(x, dropout=0.25, prefix=''):
     return(x)
 
 main_input = Input(shape=(75,75,2), name='main_input')
-#aux_input = Input(shape=(75,75,3), name='aux_input')
+aux_input = Input(shape=(75,75,3), name='aux_input')
 #aux_input_nn = Input(shape=(75,75,4), name='aux_input_nn')
 
 x1 = InputBlock(main_input, prefix='m_input')
-<<<<<<< HEAD
-#x2 = InputBlock(aux_input, prefix='a_input')
+x2 = InputBlock(aux_input, prefix='a_input')
 #x3 = model_denoise(aux_input_nn)
 #x3 = InputBlock(x3,dropout=0.3, prefix='a_input_nn')
-=======
-x2 = InputBlock(aux_input, prefix='a_input')
-x3 = model_denoise(aux_input_nn)
-x3 = InputBlock(x3,dropout=0.25, prefix='a_input_nn')
->>>>>>> 4761cbec63aa7900b08cd60db6accbcaf4e0fa84
 
-x = x1
-#x = Concatenate(axis=3)([x1,x2])
+#x = x1
+x = Concatenate(axis=3)([x1,x2])
 #x = BatchNormalization()(x)
 #x = Dropout(0.2)(x)
 
@@ -40,13 +31,13 @@ x = x1
 x = Conv2D(128, (3, 3), activation='relu')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 x = BatchNormalization()(x)
-x = Dropout(0.25)(x)
+x = Dropout(0.2)(x)
 
 #conv-block
 x = Conv2D(256, (3, 3), activation='relu')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 x = BatchNormalization()(x)
-x = Dropout(0.25)(x)
+x = Dropout(0.2)(x)
     
 #flatten
 x = Flatten()(x)
@@ -55,17 +46,17 @@ angle_input = Input(shape=[1], name='angle_input')
 merged = Concatenate()([x, angle_input])
 
 #dense-block
-x = Dense(512, activation='relu')(x)
+x = Dense(512, activation='relu')(merged)
 x = BatchNormalization()(x)
-x = Dropout(0.25)(x)
+x = Dropout(0.2)(x)
 
 #dense-block
 x = Dense(256, activation='relu')(x)
 x = BatchNormalization()(x)
-x = Dropout(0.25)(x)
+x = Dropout(0.2)(x)
 
 main_output = Dense(1, activation='sigmoid', name='main_output')(x)
-model_f = Model(inputs=[main_input,#aux_input, 
+model_f = Model(inputs=[main_input,aux_input, 
                         #aux_input_nn, 
                         angle_input], 
                         outputs=[main_output])
