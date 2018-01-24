d6169756270f5a82ba8fd82ec768de6e6588687e
Marek.K.Zielinski <ubuntu@ip-172-31-27-27.us-west-2.compute.internal>
2018-01-24 13:25:55 +0000

    prediction v28 - cnn training on 6 folds + xgboost ensembling on 150 networks overfitting

--- current_model.py
+++ current_model.py
@@ -15,52 +15,53 @@ def InputBlock(x, dropout=0.2, prefix=''):
 
 main_input = Input(shape=(75,75,2), name='main_input')
 aux_input = Input(shape=(75,75,3), name='aux_input')
-#aux_input_nn = Input(shape=(75,75,4), name='aux_input_nn')
+    #aux_input_nn = Input(shape=(75,75,4), name='aux_input_nn')
 
 x1 = InputBlock(main_input, prefix='m_input')
 x2 = InputBlock(aux_input, prefix='a_input')
-#x3 = model_denoise(aux_input_nn)
-#x3 = InputBlock(x3,dropout=0.3, prefix='a_input_nn')
+    #x3 = model_denoise(aux_input_nn)
+    #x3 = InputBlock(x3,dropout=0.3, prefix='a_input_nn')
 
-#x = x1
+    #x = x1
 x = Concatenate(axis=3)([x1,x2])
-#x = BatchNormalization()(x)
-#x = Dropout(0.2)(x)
+    #x = BatchNormalization()(x)
+    #x = Dropout(0.2)(x)
 
-#conv-block
+    #conv-block
 x = Conv2D(128, (3, 3), activation='relu')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
 
-#conv-block
+    #conv-block
 x = Conv2D(256, (3, 3), activation='relu')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
     
-#flatten
+    #flatten
 x = Flatten()(x)
 angle_input = Input(shape=[1], name='angle_input')
-#x1 = BatchNormalization()(angle_input)
+    #x1 = BatchNormalization()(angle_input)
 merged = Concatenate()([x, angle_input])
-
-#dense-block
-x = Dense(512, activation='relu')(merged)
+    
+    #dense-block
+x = Dense(513, activation='relu')(merged)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
-
-#dense-block
+    
+    #dense-block
 x = Dense(256, activation='relu')(x)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
-
+    
 main_output = Dense(1, activation='sigmoid', name='main_output')(x)
 model_f = Model(inputs=[main_input,aux_input, 
-                        #aux_input_nn, 
-                        angle_input], 
-                        outputs=[main_output])
+                            #aux_input_nn, 
+                            angle_input], 
+                            outputs=[main_output])
 
 model_f.compile(optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0),
-loss='binary_crossentropy',
-metrics=['accuracy'])
+    loss='binary_crossentropy',
+    metrics=['accuracy'])
+    
